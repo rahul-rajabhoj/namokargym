62de2e8a5064262945b48085292a4479d1a2a1b4
rahul-rajabhoj <rahulrajabhoj@gmail.com>
2017-06-10 02:55:01 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,92 @@
+$(function(){
+
+    
+    var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });    
+    $(".element").typed({
+        strings: subElementArray,
+        typeSpeed: 30,
+        contentType: 'html',
+        showCursor: false,
+        loop: true,
+        loopCount: true,
+    });
+    /* end typed element */
+
+    /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
+    ---------------------------------------------------------------------------------*/ 
+    $('.templatemo-nav').singlePageNav({
+        offset: $(".templatemo-nav").height(),
+        filter: ':not(.external)',
+        updateHash: false
+    });
+
+    /* start navigation top js */
+    $(window).scroll(function(){
+        if($(this).scrollTop()>58){
+            $(".templatemo-nav").addClass("sticky");
+        }
+        else{
+            $(".templatemo-nav").removeClass("sticky");
+        }
+    });
+    
+    /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+    /* end navigation top js */
+
+    $('body').bind('touchstart', function() {});
+
+    /* wow
+    -----------------*/
+    new WOW().init();
+});
+
+/* start preloader */
+$(window).load(function(){
+	$('.preloader').fadeOut(1000); // set duration in brackets    
+});
+/* end preloader */
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    $('html,body').animate({ scrollTop: 0 }, 'slow');
+}
+
+
+
+//Scrolling to div
+
+function ScrollTo(name) {
+  //init thread
+  ScrollToResolver(document.getElementById(name));
+}
+
+function ScrollToResolver(elem) {
+  var jump = parseInt(elem.getBoundingClientRect().top * .2);
+  document.body.scrollTop += jump;
+  document.documentElement.scrollTop += jump;
+  //lastjump detects anchor unreachable, also manual scrolling to cancel animation if scroll > jump
+  if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
+    elem.lastjump = Math.abs(jump);
+    setTimeout(function() {
+      ScrollToResolver(elem);
+    }, "50");
+  } else {
+    elem.lastjump = null;
+  }
+}
